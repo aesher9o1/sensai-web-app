996d5ebf531b881d8f4be0d9b77f918b4dee486d
Tushar Sadana <tusharsadana@gmail.com>
2020-07-08 23:29:41 +0530

    colors done, founder photo added

--- src/app/pages/home/testimonials/testimonials.component.ts
+++ src/app/pages/home/testimonials/testimonials.component.ts
@@ -21,8 +21,15 @@ export class TestimonialsComponent implements OnInit {
       name: 'Dr. Ragini Singh',
       description:
         ' It has been a really nice experience to learn with Sensai. I enrolled my child Idhant Singh under Sarvagya Singh who is a remarkable person and teacher with great aptitude for Python. He explains each and every concept with explicit detail leaving no scope for doubt. He also ensures that all concepts are lucid and I am really very proud to have my son practicing with them.',
-      designation: ' Parent of Idhant Singh',
+      designation: ' Parent of Idhant Singh, Class 8',
       image: 'ragini',
+    },
+    {
+      name: 'Mrs. Parul Tiwari',
+      description:
+        'I must say yet to come across more professional and sincere mentors like at Sensai. They understand children and come up to their level to explain the concepts. Coding is tough but they has tailored it to suit the children and make them understand easily. Sensai is always there to explain in a very approachable way. Hope this learning will help my son in the future.',
+      designation: ' Parent of Vihan Tiwari, Class 8',
+      image: 'parul',
     },
     {
       name: 'Aryan Dhiman',
