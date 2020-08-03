b7107e59cff9e86b82959929af342b2519957911
Aashis <aashiskumar986@gmail.com>
2020-08-03 21:05:01 +0530

    aptitide countdown timer set

--- src/app/pages/aptitude/aptitude.component.ts
+++ src/app/pages/aptitude/aptitude.component.ts
@@ -18,6 +18,7 @@ export class AptitudeComponent implements OnInit {
   answered: Record<string, boolean> = {};
   questionHelper = QuestionHelper.getInstance();
   quizEndTime: Date;
+  formText = 'Not ready to give this right now? Give it later!';
   @ViewChild('cd', { static: false }) private countdown: CountdownComponent;
   questions = [];
 
@@ -56,6 +57,21 @@ export class AptitudeComponent implements OnInit {
 
   startQuiz() {
     if (!this.userKey) return;
+
+    if (!this.validateEmail(this.email)) {
+      this.formText = 'The email is not valid';
+      return;
+    }
+
+    if (this.userKey.toString().length < 10) {
+      this.formText = 'The phone number is not valid';
+      return;
+    }
+
+    if (this.name.length < 3) {
+      this.formText = 'Please enter a valid name';
+      return;
+    }
     // this.countdown.begin();
     this.isQuizShow = true;
     const getEndTimeInMinutes = 15 * 60000;
@@ -66,6 +82,11 @@ export class AptitudeComponent implements OnInit {
 
   selectQuestion(sectionID: string, index: number, selectedOption: Options) {
     if (selectedOption.isCorrect) this.answered[`${sectionID}-${index}`] = true;
+  }
+
+  validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
   }
 
   submitQuiz() {
