ed6a179060fb72f908ffb2440b393735ee754321
Tushar Sadana <tusharsadana@gmail.com>
2020-08-09 17:04:59 +0530

    courses done, color to be added

--- src/app/pages/home/courses-we-teach/courses-we-teach.component.ts
+++ src/app/pages/home/courses-we-teach/courses-we-teach.component.ts
@@ -7,58 +7,141 @@ import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./courses-we-teach.component.scss'],
 })
 export class CoursesWeTeachComponent implements OnInit {
-  cards = [
+  beginner = [
     {
-      img: '2',
-      title: 'Python',
-      description:
-        'Want to learn Data Science or Machine learning? Do you have a knack for development? Python is your way towards a myriad of skills.',
-      class: 'mt-6 ml-lg-auto',
-      bg: '#feab47',
-      animation: 'fade-up',
-      link:
-        'https://docs.google.com/document/d/1lW-KPVEzdwzKQ7089mJSq-IGrsSCqZHTFkV2cYMtqxE/edit?usp=sharing',
-      payment: 'le0feb73e98e84e5b94d85c9435569edc',
+      name: 'Python',
+      lectures: '21',
+      features: [
+        '1-1 Personalised lectures',
+        'Extensive Hands on Practice',
+        'Projects and Applications',
+        'Industrial way of code',
+      ],
+      beforePrice: '9,600',
+      afterPrice: '7,200',
     },
     {
-      img: '1',
-      title: 'C++',
-      class: 'mr-lg-auto',
-      description:
-        'Skill up yourself and become a pro at the most powerful programming language in the world.',
-      bg: '#dc0065',
-      animation: 'fade-up',
-      link:
-        'https://docs.google.com/document/d/1npmJzSPOnbJdRDCPnV3g_vIvKRXEwBkU-dbv0dPIBnI/edit?usp=sharing',
-      payment: '',
+      name: 'C++',
+      lectures: '18',
+      features: [
+        '1-1 Personalised lectures',
+        'Extensive Hands on Practice',
+        'Projects and Applications',
+        'Industrial way of code',
+      ],
+      beforePrice: '9,600',
+      afterPrice: '7,200',
     },
     {
-      img: '3',
-      title: 'Internet of Things',
-      class: 'ml-lg-auto',
-      description:
-        'Learn to make intelligent machines and start your journey towards the future.',
-      bg: '#00a190',
-      animation: 'fade-up',
-      link:
-        'https://docs.google.com/document/d/1Wl0WijUIrizaJ5aYGNefkK_Ywu9r6qsI_w2FUpb8WnU/edit?usp=sharing',
-      payment: '',
-    },
-    {
-      img: '4',
-      title: 'Solidworks',
-      description:
-        'Design, Assemble, Create and become pro in Solidworks effortlessly.',
-      class: 'mt-6n mr-lg-auto',
-      bg: '#006787',
-      animation: 'fade-up',
-      link:
-        'https://docs.google.com/document/d/1g6QocKdTK6O5OXcyJcEGz7uZmLOGOzZUuYK7695FY6w/edit?usp=sharing',
-      payment: '',
+      name: 'IOT',
+      lectures: '10',
+      features: [
+        '1-1 Personalised lectures',
+        'Extensive Hands on Practice',
+        'Create a bluetooth control car',
+        'Hardware Kit included',
+      ],
+      beforePrice: '9,600',
+      afterPrice: '7,200',
     },
   ];
+  intermediate = [
+    {
+      name: 'Python',
+      lectures: '19',
+      features: [
+        '1-1 Personalised lectures',
+        'Extensive Hands on Practice',
+        'Projects and Applications',
+        'Industrial way of code',
+      ],
+      beforePrice: '9,600',
+      afterPrice: '7,200',
+    },
+    {
+      name: 'C++',
+      lectures: '18',
+      features: [
+        '1-1 Personalised lectures',
+        'Extensive Hands on Practice',
+        'Projects and Applications',
+        'Industrial way of code',
+      ],
+      beforePrice: '9,600',
+      afterPrice: '7,200',
+    },
+    {
+      name: 'Mobile App Dev',
+      lectures: '24',
+      features: [
+        '1-1 Personalised lectures',
+        'Extensive Hands on Practice',
+        'Create a To-Do List App',
+        'Create a Chat-App',
+      ],
+      beforePrice: '9,600',
+      afterPrice: '7,200',
+    },
+  ];
+  special = [
+    {
+      name: 'Machine Learning',
+      lectures: '20',
+      features: [
+        'Step in to Artifical Intelligence',
+        'Extensive Hands on Practice',
+        'Projects and Applications',
+        'Industrial way of code',
+      ],
+      beforePrice: '12,800',
+      afterPrice: '9,600',
+    },
+    {
+      name: 'Solidworks',
+      lectures: '28',
+      features: [
+        '1-1 Personalised lectures',
+        'Extensive Hands on Practice',
+        'Projects and Applications',
+        'Industrial way of code',
+      ],
+      beforePrice: '9,600',
+      afterPrice: '7,200',
+    },
+    {
+      name: 'Mobile App Dev',
+      lectures: '24',
+      features: [
+        '1-1 Personalised lectures',
+        'Extensive Hands on Practice',
+        'Create a To-Do List App',
+        'Create a Chat-App',
+      ],
+      beforePrice: '9,600',
+      afterPrice: '7,200',
+    },
+  ];
+
+  beginnerShow: boolean = true;
+  intermediateShow: boolean = false;
+  specialisationShow: boolean = false;
   longAarrowAltRight = faLongArrowAltRight;
   constructor() {}
 
   ngOnInit(): void {}
+  clickOnBeginner() {
+    this.intermediateShow = false;
+    this.specialisationShow = false;
+    this.beginnerShow = true;
+  }
+  clickOnIntermediate() {
+    this.specialisationShow = false;
+    this.beginnerShow = false;
+    this.intermediateShow = true;
+  }
+  clickOnSpec() {
+    this.beginnerShow = false;
+    this.intermediateShow = false;
+    this.specialisationShow = true;
+  }
 }
--- src/app/pages/home/header/header.component.ts
+++ src/app/pages/home/header/header.component.ts
@@ -3,13 +3,42 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  elements = [
+    {
+      icon: 'plan',
+      title: 'Improves Logics',
+      description: '',
+    },
+    {
+      icon: 'payment',
+      title: 'Better Problem Solving and Optimization',
+      description: '',
+    },
+    {
+      icon: 'work',
+      title: 'High Critical Thinking',
+      description: '',
+    },
+    {
+      icon: 'work',
+      title: 'Increases Imagination and Creativity',
+      description: '',
+    },
+    {
+      icon: 'work',
+      title: 'Nurture Focus and Concentration',
+      description: '',
+    },
+    {
+      icon: 'work',
+      title: 'One Step Forward in their Career',
+      description: '',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
